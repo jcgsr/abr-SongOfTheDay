7cc557e8341e58f0fe13344b8bb1d1c8b9508459
jcgsr <cabralguerra@gmail.com>
2022-04-15 23:37:46 -0300

    some effects

---
+++ script.js
@@ -0,0 +1,49 @@
+AOS.init();
+anime({
+  targets: ".circle",
+  translateX: 250,
+});
+
+// Wrap every letter in a span
+let textWrapper = document.querySelector(".ml6 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml6 .letter",
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i,
+  })
+  .add({
+    targets: ".ml6",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
+barba.init({
+  transitions: [
+    {
+      name: "opacity-transition",
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0,
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0,
+        });
+      },
+    },
+  ],
+});
+
+let date = new Date();
+document.querySelector("#date").innerHTML = date;
